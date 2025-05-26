d5c4f6fca050b17355de98d2733e6e357c8fe64a
bruhmoments123456 <lokientdm@gmail.com>
2025-05-26 20:41:01 +0700

    Add files via upload

---
+++ h.py
@@ -0,0 +1,37 @@
+import streamlit as st
+st.title("Intro")
+image="https://i.ytimg.com/vi/PTwI_riKzkA/maxresdefault.jpg"
+st.image(image,caption="Combat Gods 2")
+# sound=open("")
+# st.audio(sound,format="audio/mp3")
+st.title("Video")
+video="https://www.youtube.com/watch?v=Xg1XSvszGRU&t=41s"
+video2="https://www.youtube.com/watch?v=LwTu7u--vs4"
+st.video(video,format="video/mp4")
+st.video(video2,format="video/mp4")
+st.title('chill music after watch (chill guy music)')
+video1="https://www.youtube.com/watch?v=zYCLadD5n78"
+st.video(video1,format='video/mp.4')
+st.title('Final battle music')
+video22="https://www.youtube.com/watch?v=XTywMOGKh0U"
+st.video(video22,format='video/mp.4')
+col1, col2, col3, col4, col5 = st.columns(5)
+with col1:
+    b1 = st.button('Bài hát của Đen Vâu')
+with col2:
+    b2 = st.button('Bài hát của Hà anh Tuấn')    
+with col3:
+    b3 = st.button('Những bản nhạc giúp tâm trạng vui vẻ hơn')   
+if b1:
+    with st.expander('Đen Vâu'):
+        st.title('MV yêu thích')
+        #video số 1
+        st.write('Đi về nhà')
+        video = 'https://www.youtube.com/watch?v=vTJdVE_gjI0'
+        st.video(video, format='video/mp4')
+        #video số 2
+        st.write('Mang tiền về cho mẹ')
+        video_2 = 'https://www.youtube.com/watch?v=UVbv-PJXm14'
+        st.video(video_2, format='video/mp4')
+
+
