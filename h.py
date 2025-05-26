19f033d3b2c768def986ed1f9669aa90d4dae9cb
bruhmoments123456 <lokientdm@gmail.com>
2025-05-26 21:10:31 +0700

    Update h.py

--- h.py
+++ h.py
@@ -10,7 +10,6 @@ video222="https://www.youtube.com/watch?v=sxWGwpl6A1k"
 st.video(video,format="video/mp4")
 st.title("emote")
 video2="https://www.youtube.com/watch?v=sxWGwpl6A1k"
-st.video(video,format="video/mp4")
 st.video(video2,format="video/mp4")
 st.title('chill music after watch (chill guy music)')
 video1="https://www.youtube.com/watch?v=zYCLadD5n78"
